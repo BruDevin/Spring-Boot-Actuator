dfcee6d7f63088e191023e3f43ca23e5353d3a96
BruDevin <bruno.ads.2022@gmail.com>
2023-11-30 18:48:06 -0300

    Spring Boot Actuator example

---
+++ src/main/java/com/example/springrestactuator/SpringrestactuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestactuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestactuatorApplication.class, args);
+	}
+
+}
